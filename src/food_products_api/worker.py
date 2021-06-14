c3b27b372a754367b01c5c445afca2a3a2dcbfef
Thomas Leese <thomas@leese.io>
2021-06-14 21:23:30 +0100

    Use JSON serializer

--- src/food_products_api/worker.py
+++ src/food_products_api/worker.py
@@ -9,4 +9,5 @@ app = celery.Celery("food_products_api.worker")
 app.conf.update(
     BROKER_URL=os.environ["REDIS_URL"],
     CELERY_RESULT_BACKEND=os.environ["REDIS_URL"],
+    CELERY_TASK_SERIALIZER="json",
 )
